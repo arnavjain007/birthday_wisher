a9c6d1bf2db5194c24fa9a70554e3d9a8cce1218
arnavjain007 <amyjain801@gmail.com>
2025-12-15 13:50:37 +0530

    OAuth working properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import Login from './components/Login.jsx'
+import { UserContext } from './contexts/UserContext.jsx';
 import './App.css'
 
 function App() {
-  const [user,setUser]=useState([]);
+  const [user,setUser]=useState(null);
 
   return (
     <>
-      <UserContext.Provider>
-        <Login user={user} setUser={setUser}/>
+      <UserContext.Provider value={{user,setUser}}>
+        <Login/>
       </UserContext.Provider>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,43 @@
 import {jwtDecode} from "jwt-decode";
-import {useRef,useEffect} from "react";
+import {useRef,useEffect,useState,useContext} from "react";
+import { UserContext } from "../contexts/UserContext";
 
-const Login = ({user,setUser}) => {
+const Login = () => {
+  const {setUser}=useContext(UserContext);
   const googleBtnRef=useRef(null);
 
+  const [isSignup,setIsSignup]=useState(false);
+  const [formData,setFormData]=useState({
+    email:"",
+    password:"",
+    error:"",
+  });
+
+  handleChange=(e)=>{
+    const {name,value}=e.target;
+    
+  }
+
   const handleCallback=(res)=>{
-    const userData=jwtDecode(red.credential);
+    const userData=jwtDecode(res.credential);
     setUser(userData);
   }
 
-  useEffect=(()=>{
-    googleBtnRef.accounts.id.initialise({
-        client_id:"YOUR_GOOGLE_CLIENT_ID",
+  useEffect(()=>{
+    google.accounts.id.initialize({
+        client_id:"609096507330-c0el62kih7fda1to3dcjgtdjrj3cl1pe.apps.googleusercontent.com",
         callback:handleCallback,
     });
-    googleBtnRef.accounts.id.renderButton(
+    google.accounts.id.renderButton(
         googleBtnRef.current,
         {theme:"outline",size:"large"}
     );
   },[]);
 
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Sign-Up/Login</h1>
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-bold my-4">Sign-Up/Login</h1>
+
         <div ref={googleBtnRef}></div>
     </div>
   )
